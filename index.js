67d4bf2da30d554147148410f11a23142c1dbc9f
Aleksandr Roslyakov <torpid@gmail.com>
2018-02-17 03:41:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ function logWhisper (string) {
 function sayHiToGrandma (string) {
   if (string = whisper) {
     return "I can't hear you!"
-  } else if (string = shout) {
-    return "YES INDEED!"
-  } else if (string = "I love you, Grandma"){
-    return "I love you, too."
+  }
+}
+function sayHiToGrandma (string) {
+  if (string = shout) {
+    return "I can't hear you!"
   }
 }
